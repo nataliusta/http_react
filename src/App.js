6f7762b90213ff3f6553e82408e805ae7de0ff29
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-25 15:48:34 +0300

    Add error message

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.dev/api/films/')
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch('https://swapi.dev/api/film/');
+
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
+
     const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
@@ -19,6 +30,10 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   }
 
   return (
@@ -27,7 +42,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
